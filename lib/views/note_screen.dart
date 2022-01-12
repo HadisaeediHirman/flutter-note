5dd8795946c3b9c22d393468a84f9f4b79fb9a56
Ali Ebadi <ebadialideveloper@gmail.com>
2022-01-12 16:50:36 +0330

    note app bar added

--- lib/views/note_screen.dart
+++ lib/views/note_screen.dart
@@ -7,6 +7,7 @@ import 'package:simple_hive_note/theme/colors.dart';
 import 'package:simple_hive_note/views/add_update_note_screen.dart';
 import 'package:simple_hive_note/widgets/action_button.dart';
 import 'package:simple_hive_note/widgets/empty_text.dart';
+import 'package:simple_hive_note/widgets/note_appbar.dart';
 import 'package:simple_hive_note/widgets/note_card.dart';
 import '../constants.dart';
 
@@ -22,6 +23,34 @@ class NoteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: NoteAppbar(
+        title: "notes".tr,
+        actions: [
+          Obx(
+            () => controller.selectedIds.isEmpty
+                ? ActionButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.settings),
+                  )
+                : ActionButton(
+                    onPressed: () {
+                      controller.deleteNotes();
+                      context.showMessage(
+                          "${controller.selectedIds.length} یادداشت با موفقیت حذف شدند.");
+                    },
+                    child: Row(
+                      children: [
+                        Text(
+                          "حذف - ${controller.selectedIds.length}",
+                        ),
+                        const SizedBox(width: 8),
+                        const Icon(Icons.delete),
+                      ],
+                    ),
+                  ),
+          ),
+        ],
+      ),
       floatingActionButton: FadeInLeft(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
@@ -54,88 +83,37 @@ class NoteScreen extends StatelessWidget {
         ),
       ),
       body: Obx(
-        () => Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FadeInDown(
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                height: 100,
-                alignment: Alignment.bottomRight,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'notes'.tr,
-                      textAlign: TextAlign.right,
-                      style: const TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    if (controller.selectedIds.isEmpty)
-                      ActionButton(
-                        onPressed: () {},
-                        child: const Icon(Icons.settings),
-                      ),
-                    if (controller.selectedIds.isNotEmpty)
-                      ActionButton(
-                        onPressed: () {
-                          controller.deleteNotes();
-                          context.showMessage(
-                              "${controller.selectedIds.length} یادداشت با موفقیت حذف شدند.");
-                        },
-                        child: Row(
-                          children: [
-                            Text(
-                              "حذف - ${controller.selectedIds.length}",
-                            ),
-                            const SizedBox(width: 8),
-                            const Icon(Icons.delete),
-                          ],
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              child: controller.notes.isEmpty
-                  ? EmptyText(message: "empty_note".tr)
-                  : Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: StaggeredGrid.count(
-                        crossAxisCount: 2,
-                        mainAxisSpacing: 16,
-                        crossAxisSpacing: 16,
-                        children: List.generate(
-                          controller.notes.length,
-                          (index) {
-                            final note = controller.notes[index];
-                            return FadeInDown(
-                              delay: Duration(milliseconds: 150 * index),
-                              child: NoteCard(
-                                note: note,
-                                selected: controller.isSelected(note.id),
-                                onSelect: () =>
-                                    controller.toggleSelect(note.id),
-                                onTap: () {
-                                  if (controller.selectedIds.isEmpty) {
-                                    print("Navigate");
-                                  } else {
-                                    controller.toggleSelect(note.id);
-                                  }
-                                },
-                              ),
-                            );
+        () => controller.notes.isEmpty
+            ? EmptyText(message: "empty_note".tr)
+            : Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: StaggeredGrid.count(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  children: List.generate(
+                    controller.notes.length,
+                    (index) {
+                      final note = controller.notes[index];
+                      return FadeInDown(
+                        delay: Duration(milliseconds: 150 * index),
+                        child: NoteCard(
+                          note: note,
+                          selected: controller.isSelected(note.id),
+                          onSelect: () => controller.toggleSelect(note.id),
+                          onTap: () {
+                            if (controller.selectedIds.isEmpty) {
+                              print("Navigate");
+                            } else {
+                              controller.toggleSelect(note.id);
+                            }
                           },
                         ),
-                      ),
-                    ),
-            ),
-          ],
-        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
       ),
     );
   }
--- lib/widgets/note_appbar.dart
+++ lib/widgets/note_appbar.dart
@@ -1,11 +1,49 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_hive_note/theme/spacing.dart';
 
 class NoteAppbar extends StatelessWidget with PreferredSizeWidget {
-  const NoteAppbar({Key? key}) : super(key: key);
+  const NoteAppbar({Key? key, this.title, this.actions}) : super(key: key);
+
+  final String? title;
+  final List<Widget>? actions;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return FadeInDown(
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: AppSpacings.xl),
+        padding: const EdgeInsets.symmetric(vertical: AppSpacings.xl),
+        child: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              title != null
+                  ? Expanded(
+                      child: Text(
+                        title!,
+                        style: const TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  : const Spacer(),
+              if (actions != null) ...{
+                ...actions!
+                    .map(
+                      (e) => Padding(
+                        padding: const EdgeInsets.only(right: AppSpacings.l),
+                        child: e,
+                      ),
+                    )
+                    .toList(),
+              }
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
