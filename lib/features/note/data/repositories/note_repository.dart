5145ea10f8f911e4c048211cf25eb713e3f48d8e
Ali Ebadi <ebadialideveloper@gmail.com>
2022-01-13 16:46:40 +0330

    implementing note data layer

---
+++ lib/features/note/data/repositories/note_repository.dart
@@ -0,0 +1,56 @@
+import 'package:simple_hive_note/core/data/database.dart';
+import 'package:simple_hive_note/features/note/data/models/note.dart';
+import 'package:simple_hive_note/features/note/domain/repositories/note_repository.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  final DatabaseProvider _database;
+
+  NoteRepositoryImpl(this._database);
+
+  @override
+  Future addUpdateNote(Note note) async {
+    try {
+      await _database.addUpdate(note.id!, note);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future deleteMultiNotes(List<String> ids) async {
+    try {
+      await _database.deleteAll(ids);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future deleteNote(String id) async {
+    try {
+      await _database.delete(id);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  List<Note> getAllNotes() {
+    try {
+      final notes = _database.getAll().map((note) => note as Note).toList();
+      return notes;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<Note> getNote(String id) async {
+    try {
+      final note = await _database.get(id) as Note;
+      return note;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
